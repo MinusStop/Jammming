da9affbfae2747f9fecee0a42cc57a034abee988
Michael St. Clare <85242153+WrennMillstone@users.noreply.github.com>
2022-01-05 08:42:36 -0500

    remove placeholder searchresults and playlisttracks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,48 +9,10 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchResults: [
-        {
-          name: 'name1',
-          artist: 'artist1',
-          album: 'album1',
-          id: 1
-        },
-        {
-          name: 'name2',
-          artist: 'artist2',
-          album: 'album2',
-          id: 2
-        },
-        {
-          name: 'name3',
-          artist: 'artist3',
-          album: 'album3',
-          id: 3
-        }
-      ],
+      searchResults: [],
 
       playlistName: 'Test Playlist',
-      playlistTracks: [
-        {
-          name: 'playlist1',
-          artist: 'artist1',
-          album: 'album1',
-          id: 4
-        },
-        {
-          name: 'playlist2',
-          artist: 'artist2',
-          album: 'album2',
-          id: 5
-        },
-        {
-          name: 'playlist3',
-          artist: 'artist3',
-          album: 'album3',
-          id: 6
-        }
-      ],
+      playlistTracks: [],
     };
 
     this.addTrack = this.addTrack.bind(this);
